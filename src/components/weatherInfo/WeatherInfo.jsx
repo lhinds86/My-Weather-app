9063e6a8cc0f509665ad85e99d04a7ba398200bb
lhinds86 <leonhinds1986@gmail.com>
2024-09-25 20:37:08 -0400

    Added gitIgnore files

--- src/components/weatherInfo/WeatherInfo.jsx
+++ src/components/weatherInfo/WeatherInfo.jsx
@@ -1,3 +1,7 @@
+
+require('dotenv').config()
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import styles from './WeatherInfo.module.css'
 import sunrise from '../../../src/assets/sunrise-white 1.png'
 import sunset from '../../../src/assets/sunset-white 1.png'
@@ -6,29 +10,31 @@ import humidityIcon from '../../../src/assets/humidity 1.png'
 import windSpeedIcon from '../../../src/assets/wind 1.png'
 import pressureIcon from '../../../src/assets/pressure-white 1.png'
 import uvIcon from '../../../src/assets/uv-white 1.png'
+
+const APIkey = process.env.APIkey
+
 function WeatherInfo() {
-  
 
     return (
       <div className={styles.infoContainer}>
         <div className={styles.weatherContainer}>
             {/* Left Weather Info */}
             <div className={styles.leftInfo}>
-                <h2>75°F</h2>
-                <h3>Feels like: 78°F</h3>
+                <h2 id='temp'>75°F</h2>
+                <h3 id='feelsLike'>Feels like: 78°F</h3>
 
                 <div className={styles.sunrise}>
                     <img src={sunrise} alt='Sunrise icon' />
                     <div className={styles.sunriseInfo}>
                         <h4>Sunrise</h4>
-                        <p>06:37 AM</p>
+                        <p id='sunriseTime'>06:37 AM</p>
                     </div>
                 </div>
                 <div className={styles.sunset}>
                     <img src={sunset} alt='Sunset icon' />
                     <div className={styles.sunsetInfo}>
                         <h4>Sunset</h4>
-                        <p>020:37 AM</p>
+                        <p id='sunsetTime'>020:37 AM</p>
                     </div>
                 </div>
             </div>
@@ -36,14 +42,16 @@ function WeatherInfo() {
             <div className={styles.middleInfo}>
                 <form>
                     <input className={styles.searchBar} 
+                        id='locationInput'
                         name='searchBar'
                         placeholder='Location'
+                        type='text'
                         required
                         />
                 </form>
-                <h2 className={styles.locationName}>Atlanta, GA</h2>
-                <img src={partlyCloudy} alt='Partly Cloudy' />
-                <h2>Sunny</h2>
+                <h2 id='locationName' className={styles.locationName}>Atlanta, GA</h2>
+                <img id='weatherImg' src={partlyCloudy} alt='Partly Cloudy' />
+                <h2 id='weatherDiscription'>Sunny</h2>
             </div>
 
             {/* Right Weather Info */}
@@ -51,8 +59,8 @@ function WeatherInfo() {
                 <div className={styles.weatherLeft}>
                     <div className={styles.humidity}>
                         <img src={humidityIcon} alt="Humidity Icon" />
-                        <p>41%</p>
-                        <h4>Humidity</h4>
+                        <p id='humidity'>41%</p>
+                        <h4>Humidity</h4>w
                     </div>
                     <div className={styles.windSpeed}>
                         <img src={windSpeedIcon} alt="Wind Speed Icon" />
